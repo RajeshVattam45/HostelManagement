12f1ba8e9a9bc9a8bdaea9f4caf49c50a3bf11f2
RajeshVattam45 <>
2025-11-24 13:18:02 +0530

    Added sample test class

---
+++ HostelService.Tests/SampleTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostelService.Tests
+{
+    public class SampleTests
+    {
+        [Fact]
+        public void True_ShouldBe_True ( )
+        {
+            Assert.True ( true );
+        }
+    }
+}
